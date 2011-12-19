be33d7d3583a58e26bf336bf17e2df36c5f0c567
William Yanez <wyanez@gmail.com>
2011-12-19 12:02:23 -0430

    Se modificó el upload_file.php para que guarde un log del upload

--- upload_file.php
+++ upload_file.php
@@ -4,6 +4,10 @@ $tempFile = $_FILES['Filedata']['tmp_name'];
 $fileName = $_FILES['Filedata']['name'];
 $fileSize = $_FILES['Filedata']['size'];
 
-move_uploaded_file($tempFile, "/tmp" . $fileName);
+$archivo=fopen('/tmp/log.txt','w');
+fwrite($archivo,"Subiendo Archivo $tempFile ->  $fileName ($fileSize  Bytes)");
+fclose($archivo);
+
+move_uploaded_file($tempFile, "/tmp/" . $fileName);
 
 ?>
