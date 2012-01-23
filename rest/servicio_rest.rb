259154a372ead35cbdba711beac566865a2d99bc
William Yanez <william.yanez@ematics.com.ve>
2012-01-23 09:35:02 -0430

    Actualizacion del cliente REST para que trabaje con un arreglo asociativo

--- rest/servicio_rest.rb
+++ rest/servicio_rest.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 require 'json'
 
 get '/hola/:nombre' do
-    data=["Hola #{params[:nombre]}!",
-          "Hi #{params[:nombre]}!" ]
+    #data=["Hola #{params[:nombre]}!","Hi #{params[:nombre]}!" ]
+    data={:spanish => "Hola #{params[:nombre]}!",
+          :english => "Hi #{params[:nombre]}!" }
+    
     JSON.dump(data)    
 end
